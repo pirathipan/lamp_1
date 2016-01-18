3330c89191923e75ba7be1de5070482cd562d400
pirathipan <pirathipan@MacBook-Pro-de-Pirathipan.local>
2016-01-18 10:12:03 +0100

    message

--- index.php
+++ index.php
@@ -1,54 +1,99 @@
 <?php
-    session_start();
-    if(empty($_SESSION['choice']))
-    {
-        $_SESSION['choice'] = rand(0,100);
-    }
-    if(empty($_SESSION['cpt'])){
-        $_SESSION['cpt'] = 0;
-    }
-    if(empty($_SESSION['best'])){
-        
-    }
+  require_once("config/dbconfig.php");
+  session_start();
 
+
+  if(!isset($_SESSION['user'])){
+    header("Location: /login.php");
+    exit;
+  }
+
+  if(isset($_POST['reset_best'])){
+      unset($_SESSION['best_score']);
+  }
+
+  if(empty($_SESSION['choice']) || isset($_POST['reset'])){
+    $choice  =  rand(0,100);
+    $_SESSION['score'] = 0;
+    $_SESSION['choice'] = $choice;
+  }else{
     $choice = $_SESSION['choice'];
-    echo($choice);
-    $response = null;
-    if(!isset($_POST['guess']))
-    {
-        
-        $response = "pas de nombre";
+  }
+
+  $response = null;
+  global $config;
+
+  $pdo = new PDO($config['host'], $config['user'], $config['password']);
+
+  if( !isset($_POST['guess'])
+    || empty($_POST['guess'])){
+    $response = "Pas de nombre";
+  }else{
+    $guess = $_POST['guess'];
+    $_SESSION['score']++;
+    if($guess > $choice) {
+      $response = "C'est moins";
+    }elseif($guess < $choice){
+      $response = "C'est plus";
     }else{
-        $_SESSION['cpt']++;
-        $guess = $_POST['guess'];
-        echo('<br>cpt :'.$_SESSION['cpt']);
-        if($guess > $choice ){
-            $response = "c'est moins";
-        }elseif($guess < $choice){
-            $response = "c'est plus";
-        }else
-        {
-            $response = "c'est gagne";
-            unset($_SESSION['choice']);
-            unset($_SESSION['cpt']);
-        }
+      $response = "C'est gagné";
+      if( !isset($_SESSION['best_score'])
+          || $_SESSION['best_score'] > $_SESSION['score']){
+          $_SESSION['best_score'] = $_SESSION['score'];
+
+
+
+          $stmt = $pdo->prepare("UPDATE intab SET best_score = :best_score WHERE id = :id ");
+          var_dump("UPDATE intab SET best_score = :best_score WHERE id = :id ");
+          $stmt->bindParam("best_score",$_SESSION['best_score']);
+          $stmt->bindParam("id",$_SESSION['id']);
+          $stmt->execute();
+          $result = $stmt->fetch();
+      }
+
+      unset($_SESSION['choice']);
     }
+  }
 
 ?>
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title></title>
+  <meta charset="UTF-8">
+  <title>Des papiers dans un bol </title>
 </head>
 <body>
-    <form method="post">
-        <input type="text" name="guess">
-        <input type="submit">
-        <?php 
-        echo($response) ;
-        ?>
-    </form>
+
+  <?php echo $response;?> <br>
+  Nombre de coup : <?php echo $_SESSION['score']; ?><br>
+  <em>[Meilleur score pour <?php echo $_SESSION['user'];?>:
+  <?php
+    echo !isset($_SESSION['best_score'])
+          ? "Pas de meilleur score"
+          : "score :" .$_SESSION['best_score'];
+  ?></em>
+  <form method="POST">
+    <input type="text" name="guess" autofocus>
+    <input type="submit">
+    <input type="submit" name="reset" value="reset">
+    <input type="submit" name="reset_best" value="reset best">
+  </form>
+  <em>(La réponse est <?php echo $choice?>)</em>
+
+
+  <form method="POST" action="/login.php">
+    <input type="submit" name="logout" value="Logout">
+  </form>
+
+  <?php
+    $stmt = $pdo->prepare("SELECT login, best_score from intab ORDER BY `best_score` LIMIT 0,10");
+    $stmt->execute();
+    echo('<table border="1px">');
+    echo('<th>name</th><th>Score</th>');
+    while($result = $stmt->fetch()){
+        echo('<tr>'.'<td>' . $result['login'].'</td>'. '<td>' .$result['best_score'].'</td>' . '</tr>');
+      }
+    echo('</table>');
+  ?>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,60 @@
+<?php
+require_once("config/dbconfig.php");
+session_start();
+
+if(isset($_POST['logout'])){
+  unset($_SESSION['user']);
+}
+
+if(isset($_SESSION['user'])){
+    header("Location: /");
+    exit;
+}
+
+$errormessage = null;
+if(isset($_POST['username'])){
+
+  global $config;
+  $pdo = new PDO($config['host'], $config['user'], $config['password']);
+
+  $stmt = $pdo->prepare("SELECT * FROM intab
+                          WHERE login = :login"
+                        );
+
+
+  $stmt->bindParam("login",$_POST['username']);
+  $stmt->execute();
+  $result = $stmt->fetch();
+
+  if($result === false){
+    $errormessage = "Wrong username";
+  }elseif (empty($_POST["password"])) {
+    $errormessage = "No password";
+  }elseif (sha1($_POST["password"]) != $result["password"]) {
+    $errormessage = "Wrong password";
+  }else{
+    $_SESSION['user'] = $result["login"];
+    $_SESSION['best_score'] = $result['best_score'];
+    $_SESSION['id'] = $result["id"];
+    header("Location: /");
+    exit;
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  Merci de vous connecter :
+  <form method="POST">
+  Login : <input type="text" name="username"><br>
+  Password : <input type="text" name="password"><br>
+  <input type="submit" value="Log in">
+  </form>
+  <?php echo $errormessage;?>
+</body>
+</html>
